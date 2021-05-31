e446932e3d842feb0cd7503ead36237af4bb9435
uit19521881 <19521881@gm.uit.edu.vn>
2021-05-31 20:08:13 +0700

    completed design userGUI

---
+++ lab5/quanlytaikhoan_3layer/src/GUI/userGUI.java
@@ -0,0 +1,193 @@
+package GUI;
+
+/**
+ *
+ * @author THUYNGA
+ */
+public class userGUI extends javax.swing.JFrame {
+    public userGUI() {
+        initComponents();
+        setSize(600,400);
+        setTitle("Quanlytaikhoan");
+        setVisible(true);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lbltit = new javax.swing.JLabel();
+        lblUserid = new javax.swing.JLabel();
+        lblUsername = new javax.swing.JLabel();
+        lblUserpassword = new javax.swing.JLabel();
+        lblUserrole = new javax.swing.JLabel();
+        txtUserid = new javax.swing.JTextField();
+        txtUsername = new javax.swing.JTextField();
+        txtUserpassword = new javax.swing.JTextField();
+        cbbUserrole = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        btnInsert = new javax.swing.JButton();
+        btnUpdate = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAlwaysOnTop(true);
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setLocationByPlatform(true);
+        setMinimumSize(new java.awt.Dimension(600, 400));
+        setPreferredSize(new java.awt.Dimension(600, 400));
+        setSize(new java.awt.Dimension(600, 400));
+
+        lbltit.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        lbltit.setText("QUẢN LÝ TÀI KHOẢN");
+
+        lblUserid.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        lblUserid.setText("Mã nhân viên");
+
+        lblUsername.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        lblUsername.setText("Tên đăng nhập");
+
+        lblUserpassword.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        lblUserpassword.setText("Mật khẩu");
+
+        lblUserrole.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        lblUserrole.setText("Phân quyền");
+
+        txtUserid.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+
+        txtUsername.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+
+        txtUserpassword.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+
+        cbbUserrole.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        cbbUserrole.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        btnInsert.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        btnInsert.setText("Thêm");
+        btnInsert.setMaximumSize(new java.awt.Dimension(100, 25));
+        btnInsert.setMinimumSize(new java.awt.Dimension(100, 25));
+        btnInsert.setPreferredSize(new java.awt.Dimension(100, 25));
+
+        btnUpdate.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        btnUpdate.setText("Cập nhật");
+        btnUpdate.setPreferredSize(new java.awt.Dimension(100, 25));
+        btnUpdate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUpdateActionPerformed(evt);
+            }
+        });
+
+        btnDelete.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        btnDelete.setText("Xóa");
+        btnDelete.setMaximumSize(new java.awt.Dimension(100, 25));
+        btnDelete.setMinimumSize(new java.awt.Dimension(100, 25));
+        btnDelete.setPreferredSize(new java.awt.Dimension(100, 25));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblUserid)
+                            .addComponent(lblUsername))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(92, 92, 92)
+                                .addComponent(lbltit))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(txtUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(txtUserid, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(lblUserpassword)
+                                    .addComponent(lblUserrole))
+                                .addGap(22, 22, 22)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(txtUserpassword)
+                                    .addComponent(cbbUserrole, 0, 130, Short.MAX_VALUE))))))
+                .addGap(46, 46, 46))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(101, 101, 101)
+                .addComponent(btnInsert, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
+                .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35)
+                .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(113, 113, 113))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(7, 7, 7)
+                .addComponent(lbltit)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblUserid)
+                            .addComponent(txtUserid, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblUsername)
+                            .addComponent(txtUsername, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblUserpassword)
+                            .addComponent(txtUserpassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(lblUserrole)
+                            .addComponent(cbbUserrole, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addGap(41, 41, 41)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnInsert, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(77, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUpdateActionPerformed
+    }//GEN-LAST:event_btnUpdateActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnInsert;
+    private javax.swing.JButton btnUpdate;
+    private javax.swing.JComboBox<String> cbbUserrole;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JLabel lblUserid;
+    private javax.swing.JLabel lblUsername;
+    private javax.swing.JLabel lblUserpassword;
+    private javax.swing.JLabel lblUserrole;
+    private javax.swing.JLabel lbltit;
+    private javax.swing.JTextField txtUserid;
+    private javax.swing.JTextField txtUsername;
+    private javax.swing.JTextField txtUserpassword;
+    // End of variables declaration//GEN-END:variables
+}
