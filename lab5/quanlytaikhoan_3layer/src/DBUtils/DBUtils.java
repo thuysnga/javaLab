74e373034d95d5a43fa4fc606afe5ecbc46de9c8
uit19521881 <19521881@gm.uit.edu.vn>
2021-05-31 19:33:47 +0700

    create DBUtils to connect database qlsp\user

---
+++ lab5/quanlytaikhoan_3layer/src/DBUtils/DBUtils.java
@@ -0,0 +1,34 @@
+package DBUtils;
+
+/**
+ *
+ * @author THUYNGA
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtils {
+    private Connection conn;
+
+    public DBUtils() {
+    }
+    
+    public Connection createCon() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String URL = "jdbc:mysql://localhost:3306/qlsp";
+            String USER = "root";
+            String PASS = "NGA.kiu47";
+            
+            Connection conn =DriverManager.getConnection(URL, USER, PASS);
+            
+            if (conn == null)
+                System.out.println("Ket noi khong thanh cong.");
+            else
+                System.out.println("Ket noi thanh cong.");                
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
