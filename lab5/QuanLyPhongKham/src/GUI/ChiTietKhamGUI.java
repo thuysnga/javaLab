eb61a13f0cc4e86da72b31fa90a687a0de858f56
uit19521881 <19521881@gm.uit.edu.vn>
2021-06-04 17:34:52 +0700

    design GUI

--- lab5/QuanLyPhongKham/src/GUI/ChiTietKhamGUI.java
+++ lab5/QuanLyPhongKham/src/GUI/ChiTietKhamGUI.java
@@ -11,6 +11,7 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
         setTitle("Khám bệnh");
         setVisible(true);
     }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -20,23 +21,23 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
         jPanel1 = new javax.swing.JPanel();
-        lblMaBN = new javax.swing.JLabel();
-        lblMaBN1 = new javax.swing.JLabel();
-        lblMaBN2 = new javax.swing.JLabel();
-        lblMaBN3 = new javax.swing.JLabel();
-        lblMaBN4 = new javax.swing.JLabel();
-        lblMaBN5 = new javax.swing.JLabel();
-        lblMaBN6 = new javax.swing.JLabel();
+        lblTenBS = new javax.swing.JLabel();
+        lblNgayKham = new javax.swing.JLabel();
+        lblTenBN = new javax.swing.JLabel();
+        lblYeuCauKham = new javax.swing.JLabel();
+        lblKetLuan = new javax.swing.JLabel();
+        lblDSDichVu = new javax.swing.JLabel();
+        lblDSDichVuDuocChon = new javax.swing.JLabel();
         cbbBacSiKham = new javax.swing.JComboBox<>();
-        cbbBacSiKham1 = new javax.swing.JComboBox<>();
+        cbbBenhNhan = new javax.swing.JComboBox<>();
         dtcNgayKham = new com.toedter.calendar.JDateChooser();
-        txtMaBN = new javax.swing.JTextField();
-        txtMaBN1 = new javax.swing.JTextField();
+        txtYeuCauKham = new javax.swing.JTextField();
+        txtKetLuan = new javax.swing.JTextField();
         jScrollPane3 = new javax.swing.JScrollPane();
-        jTable3 = new javax.swing.JTable();
+        tbtDichVu = new javax.swing.JTable();
         jScrollPane4 = new javax.swing.JScrollPane();
-        jTable4 = new javax.swing.JTable();
-        btnDatLichKham = new javax.swing.JButton();
+        tbtDichVuDuocChon = new javax.swing.JTable();
+        btnThem = new javax.swing.JButton();
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -66,47 +67,47 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        lblMaBN.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        lblMaBN.setForeground(new java.awt.Color(0, 102, 204));
-        lblMaBN.setText("Bác sĩ khám");
+        lblTenBS.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblTenBS.setForeground(new java.awt.Color(0, 102, 204));
+        lblTenBS.setText("Bác sĩ khám");
 
-        lblMaBN1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        lblMaBN1.setForeground(new java.awt.Color(0, 102, 204));
-        lblMaBN1.setText("Ngày khám");
+        lblNgayKham.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblNgayKham.setForeground(new java.awt.Color(0, 102, 204));
+        lblNgayKham.setText("Ngày khám");
 
-        lblMaBN2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        lblMaBN2.setForeground(new java.awt.Color(0, 102, 204));
-        lblMaBN2.setText("Tên bệnh nhân");
+        lblTenBN.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblTenBN.setForeground(new java.awt.Color(0, 102, 204));
+        lblTenBN.setText("Tên bệnh nhân");
 
-        lblMaBN3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        lblMaBN3.setForeground(new java.awt.Color(0, 102, 204));
-        lblMaBN3.setText("Yêu cầu khám");
+        lblYeuCauKham.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblYeuCauKham.setForeground(new java.awt.Color(0, 102, 204));
+        lblYeuCauKham.setText("Yêu cầu khám");
 
-        lblMaBN4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        lblMaBN4.setForeground(new java.awt.Color(0, 102, 204));
-        lblMaBN4.setText("Kết luận");
+        lblKetLuan.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblKetLuan.setForeground(new java.awt.Color(0, 102, 204));
+        lblKetLuan.setText("Kết luận");
 
-        lblMaBN5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        lblMaBN5.setForeground(new java.awt.Color(0, 102, 204));
-        lblMaBN5.setText("Danh sách dịch vụ");
+        lblDSDichVu.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblDSDichVu.setForeground(new java.awt.Color(0, 102, 204));
+        lblDSDichVu.setText("Danh sách dịch vụ");
 
-        lblMaBN6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        lblMaBN6.setForeground(new java.awt.Color(0, 102, 204));
-        lblMaBN6.setText("Danh sách dịch vụ được bác sĩ chọn");
+        lblDSDichVuDuocChon.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        lblDSDichVuDuocChon.setForeground(new java.awt.Color(0, 102, 204));
+        lblDSDichVuDuocChon.setText("Danh sách dịch vụ được bác sĩ chọn");
 
         cbbBacSiKham.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
         cbbBacSiKham.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Tên bác sĩ" }));
 
-        cbbBacSiKham1.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
-        cbbBacSiKham1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Tên bệnh nhân" }));
+        cbbBenhNhan.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        cbbBenhNhan.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Tên bệnh nhân" }));
 
         dtcNgayKham.setMinSelectableDate(new java.util.Date(-62135791133000L));
 
-        txtMaBN.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        txtYeuCauKham.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
 
-        txtMaBN1.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
+        txtKetLuan.setFont(new java.awt.Font("Segoe UI", 0, 12)); // NOI18N
 
-        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+        tbtDichVu.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -117,9 +118,9 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane3.setViewportView(jTable3);
+        jScrollPane3.setViewportView(tbtDichVu);
 
-        jTable4.setModel(new javax.swing.table.DefaultTableModel(
+        tbtDichVuDuocChon.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -130,10 +131,10 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane4.setViewportView(jTable4);
+        jScrollPane4.setViewportView(tbtDichVuDuocChon);
 
-        btnDatLichKham.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        btnDatLichKham.setText("Đặt lịch khám");
+        btnThem.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnThem.setText("Thêm");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -144,43 +145,43 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(lblMaBN5, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblDSDichVu, javax.swing.GroupLayout.PREFERRED_SIZE, 128, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addComponent(lblMaBN6)
+                                .addComponent(lblDSDichVuDuocChon)
                                 .addGap(9, 9, 9))
                             .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 256, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(lblMaBN4, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblKetLuan, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(txtMaBN1))
+                            .addComponent(txtKetLuan))
                         .addGroup(jPanel1Layout.createSequentialGroup()
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                 .addGroup(jPanel1Layout.createSequentialGroup()
-                                    .addComponent(lblMaBN, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(lblTenBS, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                     .addComponent(cbbBacSiKham, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addGroup(jPanel1Layout.createSequentialGroup()
-                                    .addComponent(lblMaBN2, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(lblTenBN, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                    .addComponent(cbbBacSiKham1, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(cbbBenhNhan, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addGap(18, 18, 18)
                             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                 .addGroup(jPanel1Layout.createSequentialGroup()
-                                    .addComponent(lblMaBN3, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(lblYeuCauKham, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(txtMaBN))
+                                    .addComponent(txtYeuCauKham))
                                 .addGroup(jPanel1Layout.createSequentialGroup()
-                                    .addComponent(lblMaBN1, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(lblNgayKham, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                     .addComponent(dtcNgayKham, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE))))))
                 .addContainerGap(31, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(218, 218, 218)
-                .addComponent(btnDatLichKham)
+                .addComponent(btnThem, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -190,34 +191,34 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblMaBN)
+                            .addComponent(lblTenBS)
                             .addComponent(cbbBacSiKham, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblMaBN2)
-                            .addComponent(cbbBacSiKham1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(lblTenBN)
+                            .addComponent(cbbBenhNhan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(lblMaBN1)
+                            .addComponent(lblNgayKham)
                             .addComponent(dtcNgayKham, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(lblMaBN3)
-                            .addComponent(txtMaBN, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(lblYeuCauKham)
+                            .addComponent(txtYeuCauKham, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblMaBN4)
-                    .addComponent(txtMaBN1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(lblKetLuan)
+                    .addComponent(txtKetLuan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(42, 42, 42)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblMaBN5)
-                    .addComponent(lblMaBN6))
+                    .addComponent(lblDSDichVu)
+                    .addComponent(lblDSDichVuDuocChon))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
                     .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                 .addGap(29, 29, 29)
-                .addComponent(btnDatLichKham)
+                .addComponent(btnThem)
                 .addContainerGap(73, Short.MAX_VALUE))
         );
 
@@ -237,9 +238,9 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnDatLichKham;
+    private javax.swing.JButton btnThem;
     private javax.swing.JComboBox<String> cbbBacSiKham;
-    private javax.swing.JComboBox<String> cbbBacSiKham1;
+    private javax.swing.JComboBox<String> cbbBenhNhan;
     private com.toedter.calendar.JDateChooser dtcNgayKham;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
@@ -248,16 +249,16 @@ public class ChiTietKhamGUI extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
-    private javax.swing.JTable jTable3;
-    private javax.swing.JTable jTable4;
-    private javax.swing.JLabel lblMaBN;
-    private javax.swing.JLabel lblMaBN1;
-    private javax.swing.JLabel lblMaBN2;
-    private javax.swing.JLabel lblMaBN3;
-    private javax.swing.JLabel lblMaBN4;
-    private javax.swing.JLabel lblMaBN5;
-    private javax.swing.JLabel lblMaBN6;
-    private javax.swing.JTextField txtMaBN;
-    private javax.swing.JTextField txtMaBN1;
+    private javax.swing.JLabel lblDSDichVu;
+    private javax.swing.JLabel lblDSDichVuDuocChon;
+    private javax.swing.JLabel lblKetLuan;
+    private javax.swing.JLabel lblNgayKham;
+    private javax.swing.JLabel lblTenBN;
+    private javax.swing.JLabel lblTenBS;
+    private javax.swing.JLabel lblYeuCauKham;
+    private javax.swing.JTable tbtDichVu;
+    private javax.swing.JTable tbtDichVuDuocChon;
+    private javax.swing.JTextField txtKetLuan;
+    private javax.swing.JTextField txtYeuCauKham;
     // End of variables declaration//GEN-END:variables
 }
