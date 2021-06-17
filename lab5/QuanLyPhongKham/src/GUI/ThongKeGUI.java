b4c5777346af7f64b4720aacc2312a322b58a6fc
uit19521881 <19521881@gm.uit.edu.vn>
2021-06-17 12:22:56 +0700

    competed table bctheodv

--- lab5/QuanLyPhongKham/src/GUI/ThongKeGUI.java
+++ lab5/QuanLyPhongKham/src/GUI/ThongKeGUI.java
@@ -40,6 +40,12 @@ public class ThongKeGUI extends javax.swing.JFrame {
         }
         return conn;
     }
+    public String DinhDangTienTe(int SoTien) {
+        Locale localeEN = new Locale("en","EN");
+        NumberFormat en = NumberFormat.getInstance(localeEN);
+        String str = en.format(SoTien);
+        return str;
+    }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -162,17 +168,23 @@ public class ThongKeGUI extends javax.swing.JFrame {
         if (index == 0) {
             JOptionPane.showMessageDialog(null,"Vui lòng chọn loại báo cáo!","Thông báo",JOptionPane.INFORMATION_MESSAGE);
             return;
-        }        
+        }
+        if (datTuNgay.getDate() == null || datDenNgay.getDate() == null) {
+            JOptionPane.showMessageDialog(null,"Vui lòng chọn ngày bắt đầu và ngày kết thúc!", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+        String filename ="";
         String loaiBC = "";
-        if (index == 1) 
-            loaiBC = "BÁO CÁO DOANH THU DỊCH VỤ";
-        else
-            loaiBC = "BÁO CÁO DOANH THU THEO NGÀY";
-        
-        String filename = loaiBC;
         java.sql.Date fromdate = new java.sql.Date(datTuNgay.getDate().getTime());
         java.sql.Date todate = new java.sql.Date(datDenNgay.getDate().getTime());
-
+        if (index == 1) {
+            loaiBC = "BÁO CÁO DOANH THU DỊCH VỤ";
+            filename = "BCDTDichVuTungay" + fromdate.toString() + "Denngay" + todate.toString();
+        }
+        else {
+            loaiBC = "BÁO CÁO DOANH THU THEO NGÀY";
+            filename = "BCDTTungay" + fromdate.toString() + "Denngay" + todate.toString();
+        }
         try {
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("reports/" + filename + ".pdf"));
             document.open();
@@ -236,24 +248,121 @@ public class ThongKeGUI extends javax.swing.JFrame {
             prgNgKham.setAlignment(Element.ALIGN_CENTER);
             prgNgKham.setSpacingAfter(20);
             document.add(prgNgKham);
+            if (index == 1) {
+                Paragraph prgDichVu = new Paragraph("DOANH THU CÁC DỊCH VỤ : ",fontTieuDe2);
+                prgDichVu.setSpacingAfter(10);
+                prgDichVu.setSpacingAfter(10);
+                document.add(prgDichVu); 
 
-            try {
-                Connection conn = createConn();
-                String sql = "select dichvu.madv, tendv, thanhtien "
-                        + "from dichvu, khambenh, thuphi "
-                        + "where dichvu.madv = thuphi.madv "
-                        + "and khambenh.makb = thuphi.makb "
-                        + "and thanhtoan = 1 "
-                        + "and (ngaykham between ? and ? ) "
-                        + "group by madv";
-                PreparedStatement pres = conn.prepareStatement(sql);
-                pres.setDate(1,fromdate);
-                pres.setDate(2,todate);
-                ResultSet rs = pres.executeQuery();
-                if (rs.first()) {
+                PdfPTable tableDV = new PdfPTable(5);
+                tableDV.setWidthPercentage(80);
+                tableDV.setSpacingBefore(10);
+                tableDV.setSpacingAfter(10);
+                
+                float [] table_columnWidths = {50,120,150,150,120};
+                tableDV.setWidths(table_columnWidths);
+
+                PdfPCell cellTDTT = new PdfPCell(new Paragraph("STT",fontTieuDe4));
+                cellTDTT.setBorderColor(BaseColor.BLACK);
+                cellTDTT.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDTT.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cellTDTT.setMinimumHeight(30);
+                tableDV.addCell(cellTDTT);
+                
+                PdfPCell cellTDMaDV = new PdfPCell(new Paragraph("Mã dịch vụ",fontTieuDe4));
+                cellTDMaDV.setBorderColor(BaseColor.BLACK);
+                cellTDMaDV.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDMaDV.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                tableDV.addCell(cellTDMaDV);
+    
+                PdfPCell cellTDTenDV = new PdfPCell(new Paragraph("Tên dịch vụ",fontTieuDe4));
+                cellTDTenDV.setBorderColor(BaseColor.BLACK);
+                cellTDTenDV.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDTenDV.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                tableDV.addCell(cellTDTenDV);
+
+                PdfPCell cellTDSoTien = new PdfPCell(new Paragraph("Số tiền",fontTieuDe4));
+                cellTDSoTien.setBorderColor(BaseColor.BLACK);
+                cellTDSoTien.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDSoTien.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                tableDV.addCell(cellTDSoTien);
+                
+                PdfPCell cellTDGhiChu = new PdfPCell(new Paragraph("Ghi chú",fontTieuDe4));
+                cellTDGhiChu.setBorderColor(BaseColor.BLACK);
+                cellTDGhiChu.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDGhiChu.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                tableDV.addCell(cellTDGhiChu);
+
+                try {
+                    Connection conn = createConn();
+                    String sql = "select dichvu.madv, tendv, thanhtien "
+                            + "from dichvu, khambenh, thuphi "
+                            + "where dichvu.madv = thuphi.madv "
+                            + "and khambenh.makb = thuphi.makb "
+                            + "and thanhtoan = 1 "
+                            + "and (ngaykham between ? and ? ) "
+                            + "group by madv";
+                    PreparedStatement pres = conn.prepareStatement(sql);
+                    pres.setDate(1,fromdate);
+                    pres.setDate(2,todate);
+                    ResultSet rs = pres.executeQuery();
+                    int tt = 1;
+                    int tongTien = 0;
+                    while (rs.next()) {
+                        PdfPCell cellTT = new PdfPCell(new Paragraph(String.valueOf(tt),fontNoiDung3));
+                        cellTT.setHorizontalAlignment(Element.ALIGN_CENTER);
+                        cellTT.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                        cellTT.setMinimumHeight(20);
+                        tableDV.addCell(cellTT);
+                        
+                        PdfPCell cellMaDV = new PdfPCell(new Paragraph(rs.getString("DICHVU.MADV"),fontNoiDung3));
+                        cellMaDV.setHorizontalAlignment(Element.ALIGN_CENTER);
+                        cellMaDV.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                        tableDV.addCell(cellMaDV);
+                
+                        PdfPCell cellTenDV = new PdfPCell(new Paragraph(rs.getString("TENDV"),fontNoiDung3));
+                        cellTenDV.setHorizontalAlignment(Element.ALIGN_JUSTIFIED);
+                        cellTenDV.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                        tableDV.addCell(cellTenDV);
+
+                        PdfPCell cellThanhTien = new PdfPCell(new Paragraph(DinhDangTienTe(rs.getInt("THANHTIEN")),fontNoiDung3));
+                        cellThanhTien.setPaddingLeft(10);
+                        cellThanhTien.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                        cellThanhTien.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                        tableDV.addCell(cellThanhTien);
+
+                        PdfPCell cellGhiChu = new PdfPCell(new Paragraph("",fontNoiDung3));
+                        cellGhiChu.setHorizontalAlignment(Element.ALIGN_CENTER);
+                        cellGhiChu.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                        tableDV.addCell(cellGhiChu);
+
+                        tt++;
+                        tongTien += rs.getInt("THANHTIEN");
+                    }
+                    PdfPCell cellTongCong = new PdfPCell(new Paragraph("TỔNG CỘNG : ",fontTieuDe4));
+                    cellTongCong.setColspan(3);
+                    cellTongCong.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cellTongCong.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    cellTongCong.setMinimumHeight(20);
+                    tableDV.addCell(cellTongCong);
+                        
+                    PdfPCell cellTongTien = new PdfPCell(new Paragraph(DinhDangTienTe(tongTien),fontTieuDe4));
+                    cellTongTien.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                    cellTongTien.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    tableDV.addCell(cellTongTien);
+
+                    PdfPCell cellGhiChuTT = new PdfPCell(new Paragraph("",fontTieuDe4));
+                    cellGhiChuTT.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cellGhiChuTT.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    tableDV.addCell(cellGhiChuTT);
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
+                document.add(tableDV);
+            }
+            else if (index == 2) {
+
             }
 
             document.close();
