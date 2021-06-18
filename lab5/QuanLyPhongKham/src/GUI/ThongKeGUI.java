c90669664ab651c5eab334b2066fc3e05070cf3a
uit19521881 <19521881@gm.uit.edu.vn>
2021-06-18 07:56:22 +0500

    have some bugs in create Bar chart process

--- lab5/QuanLyPhongKham/src/GUI/ThongKeGUI.java
+++ lab5/QuanLyPhongKham/src/GUI/ThongKeGUI.java
@@ -2,13 +2,34 @@ package GUI;
 import java.io.*;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Desktop;
+import java.awt.image.BufferedImage;
 import java.sql.*;
-import javax.swing.table.DefaultTableModel;
 import java.text.NumberFormat;
 import java.util.Locale;
 import javax.swing.JOptionPane;
-
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.renderer.category.BarPainter;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.chart.renderer.category.StandardBarPainter;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.chart.renderer.AbstractRenderer;
+import org.jfree.chart.renderer.xy.AbstractXYItemRenderer;
+import org.jfree.chart.labels.XYItemLabelGenerator;
+import org.jfree.chart.labels.XYLabelGenerator;
 
 /**
  *
@@ -40,11 +61,78 @@ public class ThongKeGUI extends javax.swing.JFrame {
         }
         return conn;
     }
+    public String DinhDangNgayDMY(String ngay) {
+        String[] arrNgay = ngay.split("-");
+        String d = arrNgay[2];
+        String m = arrNgay[1];
+        String y = arrNgay[0];
+        return d + "/" + m + "/" + y;
+    }
     public String DinhDangTienTe(int SoTien) {
         Locale localeEN = new Locale("en","EN");
         NumberFormat en = NumberFormat.getInstance(localeEN);
         String str = en.format(SoTien);
         return str;
+    }
+    public BufferedImage createPieChart (DefaultPieDataset dataset) {
+        BufferedImage bufferedImage = null;
+        try {
+            JFreeChart chart = ChartFactory.createPieChart("Doanh thu các dịch vụ", dataset,false, true, false);
+            chart.getTitle().setFont(new java.awt.Font("Tahoma",java.awt.Font.BOLD, 120));
+            
+            PiePlot piePlot = (PiePlot) chart.getPlot();
+            piePlot.setShadowPaint(null);
+            piePlot.setBackgroundPaint(Color.white);
+            piePlot.setOutlinePaint(null);
+            piePlot.setLabelShadowPaint(null);
+            piePlot.setLabelBackgroundPaint(Color.white);
+            piePlot.setLabelOutlinePaint(Color.white);
+            
+            piePlot.setLabelFont(new java.awt.Font("Tahoma", java.awt.Font.ITALIC, 80));
+            piePlot.setLabelLinkStroke(new BasicStroke(10.0f));
+            
+            PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} - {2}");
+            piePlot.setLabelGenerator(labelGenerator);
+            bufferedImage = chart.createBufferedImage(4000,4000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bufferedImage;
+    }
+    public BufferedImage createBarChart(DefaultCategoryDataset dataset, String tuNgay, String denNgay) {
+        BufferedImage bufferedImage = null;
+        try {
+            JFreeChart chart = ChartFactory.createBarChart("Doanh thu từ " + tuNgay + " đến " + denNgay, "Ngày", "Doanh thu", dataset, PlotOrientation.VERTICAL, false, false, false);
+            java.awt.Font fontTieuDe = new java.awt.Font ("Tahoma", java.awt.Font.BOLD, 80);
+            java.awt.Font fontNoiDung1 = new java.awt.Font ("Tahoma", java.awt.Font.PLAIN, 80);
+            java.awt.Font fontNoiDung2 = new java.awt.Font ("Tahoma", java.awt.Font.ITALIC, 80);
+            chart.getTitle().setFont(fontTieuDe);
+            
+            CategoryPlot Plot = (CategoryPlot) chart.getPlot();
+//            ((BarRenderer) Plot.getRenderer()).setBarPainter(new StandardBarPainter());
+
+            Plot.setBackgroundPaint(Color.white);
+            Plot.setOutlinePaint(null);
+
+            CategoryItemRenderer renderer = ((CategoryPlot) chart.getPlot()).getRenderer();
+            renderer.setDefaultItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+            renderer.setDefaultItemLabelsVisible(true);
+            ItemLabelPosition position = new (ItemLabelPosition(itemLabelAnchor.OUTSIDE12, TextAnchor.TOP_CENTER));
+            renderer.setDefaultPositiveItemLabelPosition(position);
+            renderer.setDefaultItemLabelsVisible(fontNoiDung1);
+
+            CategoryAxis DomainAxis = Plot.getDomainAxis();
+            DomainAxis.setTickLabelFont(fontNoiDung1);
+            DomainAxis.setLabelFont(fontNoiDung2);
+
+            ValueAxis RangeAxis = Plot.getRangeAxis();
+            RangeAxis.setTickLabelFont(fontNoiDung1);
+            RangeAxis.setLabelFont(fontNoiDung2);
+            bufferedImage = chart.createBufferedImage(5000,3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bufferedImage;
     }
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -194,8 +282,6 @@ public class ThongKeGUI extends javax.swing.JFrame {
             document.addTitle(loaiBC);
             document.addSubject(loaiBC);
             
-            //Dinh nghia cac font chu duoc su dung
-            
             File filefontTieuDe = new File ("fonts/vuArialBold.ttf");
             BaseFont bfTieuDe = BaseFont.createFont(filefontTieuDe.getAbsolutePath(),BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
             Font fontTieuDe1 = new Font(bfTieuDe,16);
@@ -233,23 +319,15 @@ public class ThongKeGUI extends javax.swing.JFrame {
             prgTieuDe.setSpacingBefore(10);
             prgTieuDe.setSpacingAfter(10);
             document.add(prgTieuDe);
-            
-            
-            String[] arrayFromDate = fromdate.toString().split("-");
-            String ngayFromDate = arrayFromDate[2];
-            String thangFromDate = arrayFromDate[1];
-            String namFromDate = arrayFromDate[0];
-            String[] arrayToDate = todate.toString().split("-");
-            String ngayToDate = arrayToDate[2];
-            String thangToDate = arrayToDate[1];
-            String namToDate = arrayToDate[0];
 
-            Paragraph prgNgKham = new Paragraph(" Từ ngày " + ngayFromDate + "/" + thangFromDate + "/" + namFromDate + " đến ngày " + ngayToDate + "/" + thangToDate + "/" + namToDate, fontTieuDe3);
+            Paragraph prgNgKham = new Paragraph(" Từ ngày " + DinhDangNgayDMY(fromdate.toString()) + " đến ngày " + DinhDangNgayDMY(todate.toString()), fontTieuDe3);
             prgNgKham.setAlignment(Element.ALIGN_CENTER);
             prgNgKham.setSpacingAfter(20);
             document.add(prgNgKham);
             if (index == 1) {
-                Paragraph prgDichVu = new Paragraph("DOANH THU CÁC DỊCH VỤ : ",fontTieuDe2);
+                DefaultPieDataset dataset = new DefaultPieDataset();
+
+                Paragraph prgDichVu = new Paragraph("I. Doanh thu của các dịch vụ : ",fontTieuDe2);
                 prgDichVu.setSpacingAfter(10);
                 prgDichVu.setSpacingAfter(10);
                 document.add(prgDichVu); 
@@ -338,6 +416,8 @@ public class ThongKeGUI extends javax.swing.JFrame {
 
                         tt++;
                         tongTien += rs.getInt("THANHTIEN");
+                        
+                        dataset.setValue(rs.getString("TENDV"), rs.getInt("THANHTIEN"));
                     }
                     PdfPCell cellTongCong = new PdfPCell(new Paragraph("TỔNG CỘNG : ",fontTieuDe4));
                     cellTongCong.setColspan(3);
@@ -360,9 +440,166 @@ public class ThongKeGUI extends javax.swing.JFrame {
                     e.printStackTrace();
                 }
                 document.add(tableDV);
+
+                Paragraph prgTTBieuDo = new Paragraph("II. Biểu đồ phần trăm Doanh thu của các dịch vụ ",fontTieuDe2);
+                prgTTBieuDo.setSpacingAfter(10);
+                prgTTBieuDo.setSpacingAfter(10);
+                document.add(prgTTBieuDo); 
+
+                BufferedImage bufferedImage = createPieChart(dataset);
+                Image image = Image.getInstance(writer, bufferedImage, 1.0f);
+                image.setAlignment(Element.ALIGN_CENTER);
+                image.scaleAbsolute(400,400);
+                document.add(image);
             }
             else if (index == 2) {
+                DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
+                Paragraph prgDichVu = new Paragraph("I. Doanh thu từng ngày : ",fontTieuDe2);
+                prgDichVu.setSpacingAfter(10);
+                prgDichVu.setSpacingAfter(10);
+                document.add(prgDichVu); 
+
+                PdfPTable tableDTTN = new PdfPTable(4);
+                tableDTTN.setWidthPercentage(80);
+                tableDTTN.setSpacingBefore(10);
+                tableDTTN.setSpacingAfter(10);
+                
+                float [] table_columnWidths = {50,150,150,80};
+                tableDTTN.setWidths(table_columnWidths);
+
+                PdfPCell cellTDTT = new PdfPCell(new Paragraph("STT",fontTieuDe4));
+                cellTDTT.setBorderColor(BaseColor.BLACK);
+                cellTDTT.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDTT.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                cellTDTT.setMinimumHeight(30);
+                tableDTTN.addCell(cellTDTT);
+                
+                PdfPCell cellTDNgay = new PdfPCell(new Paragraph("Ngày",fontTieuDe4));
+                cellTDNgay.setBorderColor(BaseColor.BLACK);
+                cellTDNgay.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDNgay.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                tableDTTN.addCell(cellTDNgay);
+    
+                PdfPCell cellTDSoTien = new PdfPCell(new Paragraph("Số tiền",fontTieuDe4));
+                cellTDSoTien.setBorderColor(BaseColor.BLACK);
+                cellTDSoTien.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDSoTien.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                tableDTTN.addCell(cellTDSoTien);
+                
+                PdfPCell cellTDGhiChu = new PdfPCell(new Paragraph("Ghi chú",fontTieuDe4));
+                cellTDGhiChu.setBorderColor(BaseColor.BLACK);
+                cellTDGhiChu.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cellTDGhiChu.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                tableDTTN.addCell(cellTDGhiChu);
+                try {
+                    Connection conn = createConn();
+                    String sql = "select khambenh.makb, ngaykham, thanhtien "
+                            + "from khambenh, thuphi "
+                            + "where khambenh.makb = thuphi.makb "
+                            + "and thanhtoan = 1 "
+                            + "and (ngaykham between ? and ? ) "
+                            + "order by ngaykham";
+                    PreparedStatement pres = conn.prepareStatement(sql);
+                    pres.setDate(1,fromdate);
+                    pres.setDate(2,todate);
+                    ResultSet rs = pres.executeQuery();
+                    int tt = 1;
+                    int tongTien = 0;
+                    int tongCong = 0;
+                    String ngay = "";
+                    if (rs.first())
+                        ngay = DinhDangNgayDMY(new java.sql.Date(rs.getDate("ngaykham").getTime()).toString());
+                    while (rs.next()) {
+                        if (ngay.equalsIgnoreCase(DinhDangNgayDMY(new java.sql.Date(rs.getDate("ngaykham").getTime()).toString()))) {
+                            tongTien += rs.getInt("thanhtien");
+                        }
+                        else {
+                            PdfPCell cellTT = new PdfPCell(new Paragraph(String.valueOf(tt),fontNoiDung3));
+                            cellTT.setHorizontalAlignment(Element.ALIGN_CENTER);
+                            cellTT.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                            cellTT.setMinimumHeight(20);
+                            tableDTTN.addCell(cellTT);
+                            
+                            PdfPCell cellNgayKham = new PdfPCell(new Paragraph(ngay,fontNoiDung3));
+                            cellNgayKham.setHorizontalAlignment(Element.ALIGN_CENTER);
+                            cellNgayKham.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                            tableDTTN.addCell(cellNgayKham);
+
+                            PdfPCell cellTongTien = new PdfPCell(new Paragraph(DinhDangTienTe(tongTien),fontNoiDung3));
+                            cellTongTien.setPaddingLeft(10);
+                            cellTongTien.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                            cellTongTien.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                            tableDTTN.addCell(cellTongTien);
+
+                            PdfPCell cellGhiChu = new PdfPCell(new Paragraph("",fontNoiDung3));
+                            cellGhiChu.setHorizontalAlignment(Element.ALIGN_CENTER);
+                            cellGhiChu.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                            tableDTTN.addCell(cellGhiChu);
+
+                            ngay = DinhDangNgayDMY(new java.sql.Date(rs.getDate("ngaykham").getTime()).toString());
+                            tongCong += tongTien;
+                            dataset.setValue(tongTien, "Doanh thu", ngay);
+                            tongTien = rs.getInt("thanhtien");
+                            tt++;
+                        }
+                    }
+                    dataset.setValue(tongTien, "Doanh thu", ngay);
+                    PdfPCell cellTT = new PdfPCell(new Paragraph(String.valueOf(tt),fontNoiDung3));
+                    cellTT.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cellTT.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    cellTT.setMinimumHeight(20);
+                    tableDTTN.addCell(cellTT);
+                            
+                    PdfPCell cellNgayKham = new PdfPCell(new Paragraph(ngay,fontNoiDung3));
+                    cellNgayKham.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cellNgayKham.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    tableDTTN.addCell(cellNgayKham);
+
+                    PdfPCell cellTongTien = new PdfPCell(new Paragraph(DinhDangTienTe(tongTien),fontNoiDung3));
+                    cellTongTien.setPaddingLeft(10);
+                    cellTongTien.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                    cellTongTien.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    tableDTTN.addCell(cellTongTien);
+
+                    PdfPCell cellGhiChu = new PdfPCell(new Paragraph("",fontNoiDung3));
+                    cellGhiChu.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cellGhiChu.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    tableDTTN.addCell(cellGhiChu);
+    
+                    PdfPCell cellTongCong = new PdfPCell(new Paragraph("TỔNG CỘNG : ",fontTieuDe4));
+                    cellTongCong.setColspan(2);
+                    cellTongCong.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cellTongCong.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    cellTongCong.setMinimumHeight(20);
+                    tableDTTN.addCell(cellTongCong);
+                        
+                    tongCong += tongTien;
+                    PdfPCell cellTongDT = new PdfPCell(new Paragraph(DinhDangTienTe(tongCong),fontTieuDe4));
+                    cellTongDT.setHorizontalAlignment(Element.ALIGN_RIGHT);
+                    cellTongDT.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    tableDTTN.addCell(cellTongDT);
+
+                    PdfPCell cellGhiChuTT = new PdfPCell(new Paragraph("",fontTieuDe4));
+                    cellGhiChuTT.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    cellGhiChuTT.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                    tableDTTN.addCell(cellGhiChuTT);                    
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                document.add(tableDTTN);
+
+                Paragraph prgTTBieuDo = new Paragraph("II. Biểu đồ Doanh thu theo ngày",fontTieuDe2);
+                prgTTBieuDo.setSpacingAfter(10);
+                prgTTBieuDo.setSpacingAfter(10);
+                document.add(prgTTBieuDo); 
+                
+                BufferedImage bufferedImage = createBarChart(dataset, DinhDangNgayDMY(fromdate.toString()), DinhDangNgayDMY(todate.toString()));
+                Image image = Image.getInstance(writer, bufferedImage, 1.0f);
+                image.setAlignment(Element.ALIGN_CENTER);
+                image.scaleAbsolute(500,300);
+                document.add(image);
             }
 
             document.close();
