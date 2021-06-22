2982ae5b8e822381e3df49e0f8f3b1e2cd0cbd07
uit19521881 <19521881@gm.uit.edu.vn>
2021-06-22 22:38:33 +0700

    connect oracle success

--- testoracleconnect/src/testoracleconnect/Testoracleconnect.java
+++ testoracleconnect/src/testoracleconnect/Testoracleconnect.java
@@ -1,7 +1,6 @@
 package testoracleconnect;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 /**
  *
@@ -9,16 +8,20 @@ import java.sql.DriverManager;
  */
 public class Testoracleconnect {
     public static void main(String[] args) {
-        Connection con = null;
         try{
             Class.forName("oracle.jdbc.OracleDriver");
-            con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.1.118:1521:XE","SYSTEM","123456");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.1.118:1521:XE","SYSTEM","123456");
+            Statement stat = con.createStatement();
+            ResultSet rs = stat.executeQuery("select * from USER_LOGIN");
+            if (rs == null)
+                System.out.println("FAIL");
+            while (rs.next())
+                System.out.println(rs.getInt("USERLOGIN_ID")+"\t"+rs.getString("USERNAME")+"\t"+rs.getString("PASSWORD")+ "\t"+ rs.getString("ROLE"));
+            
+            con.close();
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        if (con != null)
-            System.out.println("THANH CONG");
-        else System.out.println("THATBAI");
     }
 }
